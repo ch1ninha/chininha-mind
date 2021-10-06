84e6ef701a79bf8c196aaa5307adc21a613054fb
ch1ninha <lucasgschneider02@gmail.com>
2021-10-06 17:12:58 -0300

    Passando a base
    Passando a estrutura feita durante a imersão.

---
+++ maquina.js
@@ -0,0 +1,10 @@
+function temaEscuro(){
+    document.body.classList.toggle("dark")
+    var elementoDark = document.getElementById("btn-tema-escuro")
+    
+  }
+  
+  function temaEspaco(){
+    document.body.classList.toggle("space")
+    var elementoSpace = document.getElementById("btn-tema-espaco")
+  }
